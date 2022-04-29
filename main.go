d232e6cc4cbc7dd9fad997295553431871057cd4
coha <coha@itu.dk>
2022-04-29 10:35:39 +0200

    added auth,start-,stop-recording and a interval scan

--- main.go
+++ main.go
@@ -1,17 +1,36 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io/ioutil"
-	"log"
 	"net/http"
+	"strings"
+	"time"
 )
+
+type token struct {
+	Access_token string `json:"access_token"`
+	Token_type   string `json:"token_type"`
+	Expires_in   int    `json:"expires_in"`
+	Scope        string `json:"scope"`
+	Jti          string `json:"jti"`
+}
+
+var Tokenresponse token
+
+// this code must be removed before being publish to git store it in a separate file
+//************************************************************************************************
+
+//************************************************************************************************
 
 func main() {
 
-	test()
+	getAuthToken()
 
 	start_recording()
+	go run_scrape_interval()
+	time.Sleep(time.Second * 30)
 
 	stop_recording()
 
@@ -27,27 +46,12 @@ func main() {
 	// log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
-func test() {
+func getAuthToken() {
 
-	// This is a control it calls a my supermon page allowed to all and gets the status code. it schould allways return 200 ok
-
-	//the url can be change to any http get request
-	var url = "https://app.mysupermon.com/#/authentication/login"
-
-	resp, err := http.Get(url)
-
-	if err != nil {
-		log.Fatalln(err)
-	}
-	fmt.Printf("%v : get status = %v \n\n", url, resp.Status)
-
-	//*****************************************************************
-	// this is a attemt to get a access_token from mysupermon. (to get a token it must be a post)
-
-	url = "https://app.mysupermon.com/oauth/token"
+	var url = "https://app.mysupermon.com/oauth/token"
 	method := "POST"
 
-	// INSERT NODEpade code here
+	payload := strings.NewReader(userinfo)
 
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, payload)
@@ -57,7 +61,46 @@ func test() {
 		return
 	}
 	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	// insert the req header from the note file
+	req.Header.Add("Authorization", Authinfo)
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	json.Unmarshal(body, &Tokenresponse)
+
+	fmt.Println("******************************************** Auth Token ********************************************")
+	fmt.Printf("%s : %s\n", Tokenresponse.Token_type, Tokenresponse.Access_token)
+}
+
+// here are start and stop recording but we dont have a access_token to give them so they return 401( unauthorized )
+
+func start_recording() {
+	fmt.Println("******************************************** Start Recording Interval ******************************")
+
+	url := "https://app.mysupermon.com/devaten/data/startRecording?usecaseIdentifier=this-is-api-test"
+	method := "GET"
+
+	payload := strings.NewReader("")
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("applicationIdentifier", ApplicationIdentifier)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+Tokenresponse.Access_token)
 
 	res, err := client.Do(req)
 	if err != nil {
@@ -74,22 +117,75 @@ func test() {
 	fmt.Println(string(body))
 }
 
-// here are start and stop recording but we dont have a access_token to give them so they return 401( unauthorized )
+func stop_recording() {
 
-func start_recording() {
-	resp, err := http.Get("https://app.mysupermon.com/devaten/data/startRecording")
+	url := "https://app.mysupermon.com/devaten/data/stopRecording?usecaseIdentifier=this-is-api-test&inputSource"
+	method := "GET"
+
+	payload := strings.NewReader("")
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
 
 	if err != nil {
-		log.Fatalln(err)
+		fmt.Println(err)
+		return
 	}
-	fmt.Println("we got " + resp.Status)
+	req.Header.Add("applicationIdentifier", ApplicationIdentifier)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+Tokenresponse.Access_token)
+
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println("******************************************** Stop Recording Interval *********************************")
+	fmt.Println(string(body))
 }
 
-func stop_recording() {
-	resp, err := http.Get("https://app.mysupermon.com/devaten/data/stopRecording?usecaseIdentifier=PIZZA&inputSource=PIZZA&frocefullyStop=false")
+func run_scrape_interval() {
+	for {
 
-	if err != nil {
-		log.Fatalln(err)
+		time.Sleep(5 * time.Second)
+		url := "https://app.mysupermon.com/devaten/data/getRunSituation"
+		method := "GET"
+
+		payload := strings.NewReader("")
+
+		client := &http.Client{}
+		req, err := http.NewRequest(method, url, payload)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		req.Header.Add("applicationIdentifier", ApplicationIdentifier)
+		req.Header.Add("Content-Type", "application/json")
+		req.Header.Add("Authorization", "Bearer "+Tokenresponse.Access_token)
+
+		res, err := client.Do(req)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer res.Body.Close()
+
+		body, err := ioutil.ReadAll(res.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("******************************************** Run Interval ********************************************")
+		fmt.Println(string(body))
+		fmt.Println("******************************************** Stop Interval *******************************************\n")
+
 	}
-	fmt.Println("we got " + resp.Status)
 }
