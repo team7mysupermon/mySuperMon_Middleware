bd3d201f24765337d5b0ce30069a58f4eaac9bdf
coha <coha@itu.dk>
2022-04-29 10:37:16 +0200

    Fix comment clean up

--- main.go
+++ main.go
@@ -34,16 +34,6 @@ func main() {
 
 	stop_recording()
 
-	// this is made by anton and dont know what it does thinks some docker stuff
-	// http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-	//     fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
-	// })
-
-	// http.HandleFunc("/hi", func(w http.ResponseWriter, r *http.Request){
-	//     fmt.Fprintf(w, "Hi")
-	// })
-
-	// log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 func getAuthToken() {
