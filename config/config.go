1b84ce5b5b65a8f6b9f658cb9652c6062c743ba4
Babette Bækgaard <babb@itu.dk>
2022-05-08 13:45:14 +0200

    added gaugemetric scabelone

--- config/config.go
+++ config/config.go
@@ -14,9 +14,21 @@ import (
 
 var (
 	opsProcessed = promauto.NewCounter(prometheus.CounterOpts{
-		Name: "processed_events",
+		Name: "processed_events_counter",
 		Help: "Total no. of processed events",
 	})
+
+	cpuTemp = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "cpu_temperature_celsius",
+		Help: "Current temperature of the CPU.",
+	})
+	hdFailures = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "hd_errors_total",
+			Help: "Number of hard-disk errors.",
+		},
+		[]string{"device"},
+	)
 )
 
 func record_metrics() {
@@ -30,9 +42,16 @@ func record_metrics() {
 
 }
 
+func init() {
+	// Metrics have to be registered to be exposed:
+	prometheus.MustRegister(cpuTemp)
+	prometheus.MustRegister(hdFailures)
+}
 
 func main() {
 	record_metrics()
+	cpuTemp.Set(420.69)
+	hdFailures.With(prometheus.Labels{"device":"/dev/sda"}).Inc()
 
 	http.Handle("/metrics", promhttp.Handler())
 	http.ListenAndServe(":2112", nil)
