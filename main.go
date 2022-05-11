5743efdbabddc4e2c352f2b8331b5c5e3af6aa63
SebastianVFugmann <90041204+SebastianVFugmann@users.noreply.github.com>
2022-05-11 11:21:50 +0200

    Fix/changing api calls to mikko specifications (#11)
    * Removed the use of the cofig file and added the login call
    
    * Small fix, maybe we should add the option to chose interval amount
    
    Co-authored-by: Sebastian Fugmann <sefu@itu.dk>
    -Review(alesc): No problems in the delivered code, error handling needs to be improved in polishing. (But is out of scope of this story/request)

--- main.go
+++ main.go
@@ -5,13 +5,16 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
-	"os"
 	"strings"
 	"time"
 
 	"github.com/gin-gonic/gin"
 )
 
+/*
+Struct to get all information from the bearer token returned by MySuperMon upon login
+It must match the json object that we get from MySuperMon
+*/
 type Token struct {
 	AccessToken string `json:"access_token"`
 	Type        string `json:"token_type"`
@@ -19,35 +22,58 @@ type Token struct {
 	Scope       string `json:"scope"`
 	Jti         string `json:"jti"`
 }
-type Command struct {
+
+/*
+Struct for the calls that the user makes with a Usecase and a ApplicationIdentifier.
+The information is gathered from the URL and added to the struct.
+Therefore, the end of the struct, the uri:xxx part, must match the definitions in the main method.
+*/
+type StartAndStopCommand struct {
 	Usecase               string `uri:"Usecase" binding:"required"`
 	ApplicationIdentifier string `uri:"Appiden" binding:"required"`
 }
 
-type Config struct {
-	Username              string `json:"MySuperMon_Username"`
-	Password              string `json:"MySuperMon_Password"`
-	ApplicationIdentifier string `json:"MySuperMon_ApplicationIdentifier"`
-	AuthInfo              string `json:"Auth_information"`
+/*
+Struct for the calls that the user makes with a Username and a Password.
+The information is gathered from the URL and added to the struct.
+Therefore, the end of the struct, the uri:xxx part, must match the definitions in the main method.
+*/
+type LoginCommand struct {
+	Username string `uri:"Username" binding:"required"`
+	Password string `uri:"Password" binding:"required"`
 }
 
+// The authentication token needed to be able to get the access token when logging in
+var authToken = "Basic cGVyZm9ybWFuY2VEYXNoYm9hcmRDbGllbnRJZDpsamtuc3F5OXRwNjEyMw=="
+
+/*
+This object is instantiated when the user calls the login API call
+It contains the authentication token
+*/
 var Tokenresponse Token
 
-var config Config
-
+/*
+A quit channel used to close the goroutine that scrapes the recording
+The goroutine is started when the user starts the recording
+*/
 var quit = make(chan bool)
 
 func main() {
-	go getAuthToken()
 	router := gin.Default()
-	router.GET("/Start/:Usecase/:Appiden", postStart_Usecase_Appidentifier)
-	router.GET("/Stop/:Usecase/:Appiden", postStop_Usecase_Appidentifier)
-	router.Run("localhost:8999")
 
+	// The API calls
+	router.GET("/Login/:Username/:Password", getAuthToken)
+	router.GET("/Start/:Usecase/:Appiden", startRecording)
+	router.GET("/Stop/:Usecase/:Appiden", stopRecording)
+
+	// Starts the program
+	router.Run("localhost:8999")
 }
 
-func postStart_Usecase_Appidentifier(c *gin.Context) {
-	var command Command
+func startRecording(c *gin.Context) {
+	// Creates the command structure by taking information from the URL call
+	// TODO: Handle errors
+	var command StartAndStopCommand
 	if err := c.ShouldBindUri(&command); err != nil {
 		c.JSON(400, gin.H{"msg": err})
 		return
@@ -55,19 +81,69 @@ func postStart_Usecase_Appidentifier(c *gin.Context) {
 	var res = Operation(command.Usecase, "start", command.ApplicationIdentifier)
 	c.JSON(res.StatusCode, gin.H{"Control": "A recording has now started"})
 
-	go run_scrape_interval(command)
-
+	// Starts the scraping on a seperat thread
+	go scrapeWithInterval(command)
 }
 
-func postStop_Usecase_Appidentifier(c *gin.Context) {
-	var command Command
+func stopRecording(c *gin.Context) {
+	// Creates the command structure by taking information from the URL call
+	// TODO: Handle errors
+	var command StartAndStopCommand
 	if err := c.ShouldBindUri(&command); err != nil {
 		c.JSON(400, gin.H{"msg": err})
 		return
 	}
+	// Sends true through the quit channel to the goroutine that is scraping the recording
 	quit <- true
+
 	var res = Operation(command.Usecase, "stop", command.ApplicationIdentifier)
 	c.JSON(res.StatusCode, gin.H{"Control": "A recording has now ended"})
+}
+
+func getAuthToken(c *gin.Context) {
+	var url = "https://app.mysupermon.com/oauth/token"
+	method := "POST"
+
+	// Creates the command structure by taking information from the URL call
+	// TODO: Handle errors
+	var command LoginCommand
+	if err := c.ShouldBindUri(&command); err != nil {
+		c.JSON(400, gin.H{"msg": err})
+		return
+	}
+
+	// Generates the user info string
+	payload := strings.NewReader(generateUserInfo(command.Username, command.Password))
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	// TODO: Handle all errors
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Authorization", authToken)
+
+	// TODO: Handle all errors
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
+
+	// TODO: Handle all errors
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	json.Unmarshal(body, &Tokenresponse)
+
+	fmt.Println("******************************************** Auth Token ********************************************")
+	fmt.Printf("%s : %s\n", Tokenresponse.Type, Tokenresponse.AccessToken)
 }
 
 func Operation(usecase string, action string, applicationIdentifier string) *http.Response {
@@ -79,6 +155,7 @@ func Operation(usecase string, action string, applicationIdentifier string) *htt
 	client := &http.Client{}
 	req, err := http.NewRequest(method, url, payload)
 
+	// TODO: Handle errors
 	if err != nil {
 		fmt.Println(err)
 		return &http.Response{
@@ -99,6 +176,7 @@ func Operation(usecase string, action string, applicationIdentifier string) *htt
 	}
 	defer res.Body.Close()
 
+	// TODO: Handle errors
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
 		fmt.Println(err)
@@ -114,62 +192,11 @@ func Operation(usecase string, action string, applicationIdentifier string) *htt
 
 }
 
-func getAuthToken() {
-	readConfig()
-
-	for {
-		var url = "https://app.mysupermon.com/oauth/token"
-		method := "POST"
-
-		payload := strings.NewReader(generateUserInfo(config.Username, config.Password))
-
-		client := &http.Client{}
-		req, err := http.NewRequest(method, url, payload)
-
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-		req.Header.Add("Authorization", config.AuthInfo)
-
-		res, err := client.Do(req)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		defer res.Body.Close()
-
-		body, err := ioutil.ReadAll(res.Body)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-		json.Unmarshal(body, &Tokenresponse)
-
-		fmt.Println("******************************************** Auth Token ********************************************")
-		fmt.Printf("%s : %s\n", Tokenresponse.Type, Tokenresponse.AccessToken)
-
-		time.Sleep(time.Second * (time.Duration(Tokenresponse.ExpiresIn) - 100))
-	}
-}
-
-func readConfig() {
-	body, err := os.ReadFile("Devaten.conf")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	json.Unmarshal(body, &config)
-}
-
-func generateUserInfo(username string, password string) string {
-	var userInfo = "username=" + username + "&password=" + password + "&grant_type=password"
-
-	return userInfo
-}
-
-func run_scrape_interval(command Command) {
+/*
+The function that is called when the user starts the recording
+Will every 5 seconds do the run operation, which returns some information about the current recording
+*/
+func scrapeWithInterval(command StartAndStopCommand) {
 	for {
 		select {
 		case <-quit:
@@ -180,4 +207,11 @@ func run_scrape_interval(command Command) {
 		time.Sleep(5 * time.Second)
 
 	}
+}
+
+// Takes a username and a password and generates the string that is needed to login
+func generateUserInfo(username string, password string) string {
+	var userInfo = "username=" + username + "&password=" + password + "&grant_type=password"
+
+	return userInfo
 }
