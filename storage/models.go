4f266851ec212f31a3d998dbbd25ca9901ad1236
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2022-05-12 21:14:20 +0200

    Added extra structs for Run data.
    Co-authored-by: aHjelmgaard <aHjelmgaard@users.noreply.github.com>
    Co-authored-by: MaCoHa <MaCoHa@users.noreply.github.com>

--- storage/models.go
+++ storage/models.go
@@ -1,5 +1,7 @@
 package storage
 
+import "time"
+
 /*
 	Struct to get all information from the bearer token returned by MySuperMon upon login
 	It must match the json object that we get from MySuperMon
@@ -30,4 +32,46 @@ type StartAndStopCommand struct {
 type LoginCommand struct {
 	Username string `uri:"Username" binding:"required"`
 	Password string `uri:"Password" binding:"required"`
+}
+
+type RunIdentificationData struct {
+	IdNumber              int             `json:"idNum"`
+	UsecaseIdentifier     string          `json:"usecaseIdentifier"`
+	ApplicationID         int             `json:"applicationId"`
+	ApplicationName       string          `json:"applicationName"`
+	ApplicationIdentifier string          `json:"applicationIdentifier"`
+	RunSituationResult    []RunClientData `json:"runSituationResult"`
+}
+
+type RunClientData struct {
+	DataSourceId int              `json:"dataSourceId"`
+	DatabaseType string           `json:"databaseType"`
+	DatabaseName string           `json:"databaseName"`
+	SchemaName   string           `json:"schemaName"`
+	HostURL      string           `json:"hostUrl"`
+	Data         RunRecordingData `json:"data"`
+}
+
+type RunRecordingData struct {
+	SumRowsAffected        int        `json:"SUM_ROWS_AFFECTED"`
+	SumSelectRange         int        `json:"SUM_SELECT_RANGE"`
+	SumLockTime            float32    `json:"SUM_LOCK_TIME"`
+	SumSortRows            int        `json:"SUM_SORT_ROWS"`
+	SumErrors              int        `json:"SUM_ERRORS"`
+	SumRowsSent            *int       `json:"SUM_ROWS_SENT"`
+	SumSelectScan          int        `json:"SUM_SELECT_SCAN"`
+	SumNoGoodIndexUsed     int        `json:"SUM_NO_GOOD_INDEX_USED"`
+	ExecTimeMax            *time.Time `json:"EXEC_TIME_MAX"`
+	SumSortScan            int        `json:"SUM_SORT_SCAN"`
+	SumSelectRangeCheck    int        `json:"SUM_SELECT_RANGE_CHECK"`
+	SumTimerWait           float64    `json:"SUM_TIMER_WAIT"`
+	UsecaseIdentifier      string     `json:"USECASE_IDENTIFIER"`
+	StartTimeStamp         time.Time  `json:"STARTTIMESTMAP"`
+	SumRowsExamined        *int       `json:"SUM_ROWS_EXAMINED"`
+	SumSelectFullJoin      int        `json:"SUM_SELECT_FULL_JOIN"`
+	SumNoIndexUsed         int        `json:"SUM_NO_INDEX_USED"`
+	CountStar              int        `json:"COUNT_STAR"`
+	SumSelectFullRangeJoin int        `json:"SUM_SELECT_FULL_RANGE_JOIN"`
+	SumSortMergePasses     int        `json:"SUM_SORT_MERGE_PASSES"`
+	SumSortRange           int        `json:"SUM_SORT_RANGE"`
 }
