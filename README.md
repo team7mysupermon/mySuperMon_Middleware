6201d531fcd1712696d72f37c0648a1afe62734a
Anton Palm Folkmann <69316986+antonPalmFolkmann@users.noreply.github.com>
2022-05-24 16:41:00 +0200

    Revert "Merge branch 'main' into GD_146_push_2_prom"
    This reverts commit edd1b9bdd142bdd10ebceff0fb7222da6f44a4ad, reversing
    changes made to 28b1ec110fd2a22cfca49cca490864a919a0f118.

--- README.md
+++ README.md
@@ -1,54 +1,54 @@
 [![Go Report Card](https://goreportcard.com/badge/github.com/team7mysupermon/mySuperMon_Middleware)](https://goreportcard.com/report/github.com/team7mysupermon/mySuperMon_Middleware)
 
-# **mySuperMon_Middleware**
+# mySuperMon_Middleware
+This middleware was created to have an easy to set up link between MySuperMon and Prometheus.
 
-This middleware was created for complementing mySuperMon’s monitoring functionalities by integrating Prometheus.
+This middleware helps you, the user, moniter your database. Through MySuperMon and Prometheus all the relevant information about different tasks performed on your database will be monitored and saved, and this information is easily accessable through the Prometheus and MySuperMon dashboard.
 
-All the relevant information from monitoring database tests will be saved and be easily accessible via Prometheus’ and mySuperMon’s dashboards.
+Further down this document, you can find a guide on how to install, run and use this middleware.
 
-Information regarding installing and operating the middleware is available later in this document.
+You must have a MySuperMon account to use this middleware. You can create an account on their [website](https://mysupermon.com/).
 
-Utilizing this software requires a mySuperMon account, which can be created at their [website](https://mysupermon.com/).
+## How to Install
 
-## **How to Install**
+## How to Run
 
-## **How to Run**
+## How to Use
 
-## **How to Use**
+When the docker image is running, it is running on the local port **8999**, which is the port you can use to start and stop a MySuperMon recording.
 
-Once the docker image is running, local port **8999** is used for operating mySuperMon recordings.
+Also, the image will open the port **9090** that can be used to access information about the recording through **Prometheus.**
 
-Additionally, the image will open port **9090** which is used for accessing information about the recording via **Prometheus.**
+Once the middleware is up and running, you can do the following API calls:
 
-Once the middleware is up and running, the following API calls are available:
+### Login
 
-### **Login**
-
-```yaml
+```
 localhost:8999/Login/{MySuperMon Username}/{MySuperMon Password}
 ```
 
-This **must** **be the first** API call, or it will not be possible to start or stop a recording.
+This call must be made as the first API call, or it will not be possible to start or stop a recording.
 
 Once you have logged in, you can start and stop recordings without having to log in again.
 
 If the image is ever shut down, you must log in again when you restart the program.
 
-### **Start Recording**
+### Start Recording
 
-```yaml
-localhost:8999/Start/{Usecase name}/{Application Identifier}
 ```
-`Usecase name` can be anything that you choose.
-
-`Application Identifier` can be found in mySuperMon, under *Applications* → *Application*
-
-### **Stop Recording**
-
-```yaml
 localhost:8999/Start/{Usecase name}/{Application Identifier}
 ```
 
-`Usecase name` **must** be the same as the name used to start the recording.
+**Usecase name** can be anything that you choose.
 
-The `application Identifier` **must** be the same as the application identifier used to start the recording.
+**Application Identifier** can be found in MySuperMon, under *Applications* and *Application Management.*
+
+### Stop Recording
+
+```
+localhost:8999/Start/{Usecase name}/{Application Identifier}
+```
+
+**Usecase name** has to be the same as the name used to start the recording.
+
+**Application Identifier** has to be the same as the application identifier used to start the recording.
--- main.go
+++ main.go
@@ -1,10 +1,34 @@
 package main
 
 import (
-	"github.com/team7mysupermon/mySuperMon_Middleware/recording"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"time"
+
+	"github.com/team7mysupermon/mySuperMon_Middleware/storage"
 
 	"github.com/gin-gonic/gin"
 	"github.com/team7mysupermon/mySuperMon_Middleware/monitoring"
+)
+
+var (
+	// The authentication token needed to be able to get the access token when logging in
+	authToken = "Basic cGVyZm9ybWFuY2VEYXNoYm9hcmRDbGllbnRJZDpsamtuc3F5OXRwNjEyMw=="
+
+	/*
+		Instantiated when a user calls the login API call.
+		Contains the authentication token
+	*/
+	Tokenresponse storage.Token
+
+	/*
+		Closes the goroutine that scrapes the recording.
+		The goroutine is started when the user starts the recording
+	*/
+	quit = make(chan bool)
 )
 
 func main() {
@@ -13,9 +37,9 @@ func main() {
 	router := gin.Default()
 
 	// The API calls
-	router.GET("/Login/:Username/:Password", recording.GetAuthToken)
-	router.GET("/Start/:Usecase/:Appiden", recording.StartRecording)
-	router.GET("/Stop/:Usecase/:Appiden", recording.StopRecording)
+	router.GET("/Login/:Username/:Password", getAuthToken)
+	router.GET("/Start/:Usecase/:Appiden", startRecording)
+	router.GET("/Stop/:Usecase/:Appiden", stopRecording)
 
 	// Starts the program
 	err := router.Run(":8999")
@@ -24,13 +48,154 @@ func main() {
 	}
 }
 
+func startRecording(c *gin.Context) {
+	// Creates the command structure by taking information from the URL call
+	// TODO: Handle errors
+	var command storage.StartAndStopCommand
+	if err := c.ShouldBindUri(&command); err != nil {
+		c.JSON(400, gin.H{"msg": err})
+		return
+	}
+	var res = Operation(command.Usecase, "start", command.ApplicationIdentifier)
+	c.JSON(res.StatusCode, gin.H{"Control": "A recording has now started"})
 
+	// Starts the scraping on a seperat thread
+	go scrapeWithInterval(command)
+}
 
+func stopRecording(c *gin.Context) {
+	// Creates the command structure by taking information from the URL call
+	// TODO: Handle errors
+	var command storage.StartAndStopCommand
+	if err := c.ShouldBindUri(&command); err != nil {
+		c.JSON(400, gin.H{"msg": err})
+		return
+	}
+	// Sends true through the quit channel to the goroutine that is scraping the recording
+	quit <- true
 
+	var res = Operation(command.Usecase, "stop", command.ApplicationIdentifier)
+	c.JSON(res.StatusCode, gin.H{"Control": "A recording has now ended"})
+}
 
+func getAuthToken(c *gin.Context) {
+	var url = "https://app.mysupermon.com/oauth/token"
+	method := "POST"
 
+	// Creates the command structure by taking information from the URL call
+	// TODO: Handle errors
+	var command storage.LoginCommand
+	if err := c.ShouldBindUri(&command); err != nil {
+		c.JSON(400, gin.H{"msg": err})
+		return
+	}
 
+	// Generates the user info string
+	payload := strings.NewReader(generateUserInfo(command.Username, command.Password))
 
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
 
+	// TODO: Handle all errors
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("Authorization", authToken)
 
+	// TODO: Handle all errors
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer res.Body.Close()
 
+	// TODO: Handle all errors
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	err = json.Unmarshal(body, &Tokenresponse)
+	if err != nil {
+		return
+	}
+
+	fmt.Println("******************************************** Auth Token ********************************************")
+	fmt.Printf("%s : %s\n", Tokenresponse.Type, Tokenresponse.AccessToken)
+}
+
+func Operation(usecase string, action string, applicationIdentifier string) *http.Response {
+	url := "https://app.mysupermon.com/devaten/data/operation?usecaseIdentifier=" + usecase + "&action=" + action
+	method := "GET"
+
+	payload := strings.NewReader("")
+
+	client := &http.Client{}
+	req, err := http.NewRequest(method, url, payload)
+
+	// TODO: Handle errors
+	if err != nil {
+		fmt.Println(err)
+		return &http.Response{
+			Status:     err.Error(),
+			StatusCode: 500,
+		}
+	}
+	req.Header.Add("applicationIdentifier", applicationIdentifier)
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("Authorization", "Bearer "+Tokenresponse.AccessToken)
+	res, err := client.Do(req)
+	if err != nil {
+		fmt.Println(err)
+		return &http.Response{
+			Status:     err.Error(),
+			StatusCode: 500,
+		}
+	}
+	defer res.Body.Close()
+
+	// TODO: Handle errors
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+		return &http.Response{
+			Status:     err.Error(),
+			StatusCode: 500,
+		}
+	}
+
+	monitoring.ParseBody(body, action)
+
+	fmt.Printf("********************************************************** begin %v \n", action)
+	fmt.Println(string(body))
+	fmt.Printf("********************************************************** end %v \n\n", action)
+	return res
+
+}
+
+/*
+The function that is called when the user starts the recording
+Will every 5 seconds do the run operation, which returns some information about the current recording
+*/
+func scrapeWithInterval(command storage.StartAndStopCommand) {
+	for {
+		select {
+		case <-quit:
+			return
+		default:
+			Operation(command.Usecase, "run", command.ApplicationIdentifier)
+		}
+		time.Sleep(5 * time.Second)
+
+	}
+}
+
+// Takes a username and a password and generates the string that is needed to login
+func generateUserInfo(username string, password string) string {
+	var userInfo = "username=" + username + "&password=" + password + "&grant_type=password"
+
+	return userInfo
+}
